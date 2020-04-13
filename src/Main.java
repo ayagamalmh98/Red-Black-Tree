b30a8be057e4c5d22a3044922292fe6611ae2278
linhahmed <45941034+linhahmed@users.noreply.github.com>
2020-04-13 20:58:32 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import eg.edu.alexu.csd.filestructure.redblacktree.IRedBlackTree;
+import eg.edu.alexu.csd.filestructure.redblacktree.RedBlackTree;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		IRedBlackTree RedBlackTree = new RedBlackTree<>();
+		
+
+	}
+
+}
